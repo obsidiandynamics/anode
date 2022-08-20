967e0151e208cf82e34e87d51b88133f85011e11
Emil Koutanov <ekoutanov@gmail.com>
2022-08-21 09:27:55 +1000

    Added bench to tests

--- libmutex/src/multilock/tr_tests.rs
+++ libmutex/src/multilock/tr_tests.rs
@@ -3,11 +3,12 @@
 use super::MultiLock;
 use crate::test_utils;
 use crate::test_utils::{CHECK_WAIT, LONG_WAIT, SHORT_WAIT};
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::atomic::{AtomicBool, AtomicU64, Ordering};
 use std::sync::{Arc, Barrier};
 use std::thread;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 use test_utils::FAIRNESS_VARIANTS;
+use crate::multilock::{Fairness, LockReadGuard, LockWriteGuard, UpgradeOutcome};
 
 #[test]
 fn read_release_cycle() {
@@ -484,4 +485,159 @@ fn competing_multiple_write_acquire_while_read_locked() {
             t_2.join().unwrap();
         }
     }
+}
+
+// TODO other tests, non-Transram, dumped here for convenience (move later)
+
+
+#[test]
+fn micro_bench() {
+    for fairness in FAIRNESS_VARIANTS {
+        __micro_bench(fairness.into());
+    }
+}
+
+fn __micro_bench(fairness: Fairness) {
+    fn read_eventually<T>(lock: &MultiLock<T>, duration: Duration) -> LockReadGuard<T> {
+        let mut val = None;
+        while val.is_none() {
+            val = lock.try_read(duration);
+        }
+        val.unwrap()
+    }
+
+    fn write_eventually<T>(lock: &MultiLock<T>, duration: Duration) -> LockWriteGuard<T> {
+        let mut val = None;
+        while val.is_none() {
+            val = lock.try_write(duration);
+        }
+        val.unwrap()
+    }
+
+    let num_readers = 4;
+    let num_writers = 4;
+    let num_downgraders = 4;
+    let num_upgraders = 4;
+    let iterations = 1000;
+
+    let read_timeout = Duration::MAX;//Duration::from_millis(10);
+    let write_timeout = Duration::MAX;//Duration::from_millis(10);
+    let upgrade_timeout = Duration::ZERO;//Duration::from_millis(1);
+
+    let debug_locks = false;
+    let debug_exits = false;
+    let sleep_time = Duration::from_millis(0);
+
+    let protected = Arc::new(MultiLock::new(0, fairness));
+
+    let mut threads = Vec::with_capacity(num_readers + num_writers + num_downgraders);
+    let upgrade_timeouts = Arc::new(AtomicU64::default());
+    let start_time = Instant::now();
+    for i in 0..num_readers {
+        let protected = protected.clone();
+        threads.push(thread::spawn(move || {
+            let mut last_val = 0;
+            for _ in 0..iterations {
+                {
+                    let val = read_eventually(&protected, read_timeout);
+                    if debug_locks { println!("reader {i} read-locked"); }
+                    if *val < last_val {
+                        panic!("Error in reader: value went from {last_val} to {val}", val = *val);
+                    }
+                    last_val = *val;
+                    if debug_locks { println!("reader {i} read-unlocked"); }
+                }
+                thread::sleep(sleep_time);
+            }
+            if debug_exits { println!("reader {i} exited"); }
+        }))
+    }
+
+    for i in 0..num_writers {
+        let protected = protected.clone();
+        threads.push(thread::spawn(move || {
+            for _ in 0..iterations {
+                {
+                    let mut val = write_eventually(&protected, write_timeout);
+                    if debug_locks { println!("writer {i} write-locked"); }
+                    *val += 1;
+                    if debug_locks { println!("writer {i} write-unlocked"); }
+                }
+                thread::sleep(sleep_time);
+            }
+            if debug_exits { println!("writer {i} exited"); }
+        }))
+    }
+
+    for i in 0..num_downgraders {
+        let protected = protected.clone();
+        let i = i;
+        threads.push(thread::spawn(move || {
+            let mut last_val = 0;
+            for _ in 0..iterations {
+                {
+                    let mut val = protected.write();
+                    if debug_locks { println!("downgrader {i} write-locked"); }
+                    *val += 1;
+
+                    let val = val.downgrade();
+                    if debug_locks { println!("downgrader {i} downgraded"); }
+                    if *val < last_val {
+                        panic!("Error in downgrader: value went from {last_val} to {val}", val = *val);
+                    }
+                    last_val = *val;
+                    if debug_locks { println!("downgrader {i} read-unlocked"); }
+                }
+                thread::sleep(sleep_time);
+            }
+            if debug_exits { println!("downgrader {i} exited"); }
+        }))
+    }
+
+    for i in 0..num_upgraders {
+        let protected = protected.clone();
+        let i = i;
+        let upgrade_timeouts = upgrade_timeouts.clone();
+        threads.push(thread::spawn(move || {
+            let mut last_val = 0;
+            for _ in 0..iterations {
+                {
+                    let val = read_eventually(&protected, read_timeout);
+                    if debug_locks { println!("upgrader {i} read-locked"); }
+                    if *val < last_val {
+                        panic!("Error in reader: value went from {last_val} to {val}", val = *val);
+                    }
+                    last_val = *val;
+
+                    let val = val.try_upgrade(upgrade_timeout);
+                    match val {
+                        UpgradeOutcome::Upgraded(mut val) => {
+                            if debug_locks { println!("upgrader {i} upgraded"); }
+                            *val += 1;
+                            if debug_locks { println!("upgrader {i} write-unlocked"); }
+                        },
+                        UpgradeOutcome::Unchanged(_) => {
+                            upgrade_timeouts.fetch_add(1, Ordering::Relaxed);
+                            if debug_locks { println!("upgrader {i} upgrade timed out"); }
+                        }
+                    }
+                }
+                thread::sleep(sleep_time);
+            }
+            if debug_exits { println!("upgrader {i} exited"); }
+        }))
+    }
+
+    for thread in threads {
+        thread.join().unwrap();
+    }
+    let upgrade_timeouts = upgrade_timeouts.load(Ordering::Relaxed);
+    let expected_writes = ((num_writers + num_downgraders + num_upgraders) * iterations) as u64 - upgrade_timeouts;
+    assert_eq!(expected_writes, *protected.read());
+
+    let time_taken = (Instant::now() - start_time).as_secs_f64();
+    let ops = (num_readers + num_writers + 2 * num_downgraders + 2 * num_upgraders) * iterations;
+    let rate = (ops as f64) / time_taken;
+    println!("{ops} ops took {time_taken:.3} seconds; {rate:.3} ops/s");
+    println!("upgrade timeouts: {upgrade_timeouts:?}");
 }
--- libmutex/src/xlock/tests.rs
+++ libmutex/src/xlock/tests.rs
@@ -1,6 +1,9 @@
-use std::time::Duration;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicU64, Ordering};
+use std::thread;
+use std::time::{Duration, Instant};
 use crate::xlock::locklike::{LockBox, LockBoxSized};
-use crate::xlock::{ReadBiased, XLock};
+use crate::xlock::{LockReadGuard, LockUpgradeOutcome, LockWriteGuard, ReadBiased, Spec, XLock};
 
 #[test]
 fn box_cycle() {
@@ -81,4 +84,150 @@ fn box_sized_into_inner() {
     let lock = XLock::<_, ReadBiased>::new(42);
     let boxed: LockBoxSized<_> = Box::new(lock);
     assert_eq!(42, boxed.into_inner());
+}
+
+#[test]
+fn micro_bench() {
+    fn read_eventually<T, S: Spec>(lock: &XLock<T, S>, duration: Duration) -> LockReadGuard<T, S> {
+        let mut val = None;
+        while val.is_none() {
+            val = lock.try_read(duration);
+        }
+        val.unwrap()
+    }
+
+    fn write_eventually<T, S: Spec>(lock: &XLock<T, S>, duration: Duration) -> LockWriteGuard<T, S> {
+        let mut val = None;
+        while val.is_none() {
+            val = lock.try_write(duration);
+        }
+        val.unwrap()
+    }
+
+    let num_readers = 4;
+    let num_writers = 4;
+    let num_downgraders = 4;
+    let num_upgraders = 4;
+    let iterations = 1000;
+
+    let read_timeout = Duration::MAX;//Duration::from_millis(10);
+    let write_timeout = Duration::MAX;//Duration::from_millis(10);
+    let upgrade_timeout = Duration::ZERO;//Duration::from_millis(1);
+
+    let debug_locks = false;
+    let debug_exits = false;
+    let sleep_time = Duration::from_millis(0);
+
+    let protected = Arc::new(XLock::<_, ReadBiased>::new(0));
+
+    let mut threads = Vec::with_capacity(num_readers + num_writers + num_downgraders);
+    let upgrade_timeouts = Arc::new(AtomicU64::default());
+    let start_time = Instant::now();
+    for i in 0..num_readers {
+        let protected = protected.clone();
+        threads.push(thread::spawn(move || {
+            let mut last_val = 0;
+            for _ in 0..iterations {
+                {
+                    let val = read_eventually(&protected, read_timeout);
+                    if debug_locks { println!("reader {i} read-locked"); }
+                    if *val < last_val {
+                        panic!("Error in reader: value went from {last_val} to {val}", val = *val);
+                    }
+                    last_val = *val;
+                    if debug_locks { println!("reader {i} read-unlocked"); }
+                }
+                thread::sleep(sleep_time);
+            }
+            if debug_exits { println!("reader {i} exited"); }
+        }))
+    }
+
+    for i in 0..num_writers {
+        let protected = protected.clone();
+        threads.push(thread::spawn(move || {
+            for _ in 0..iterations {
+                {
+                    let mut val = write_eventually(&protected, write_timeout);
+                    if debug_locks { println!("writer {i} write-locked"); }
+                    *val += 1;
+                    if debug_locks { println!("writer {i} write-unlocked"); }
+                }
+                thread::sleep(sleep_time);
+            }
+            if debug_exits { println!("writer {i} exited"); }
+        }))
+    }
+
+    for i in 0..num_downgraders {
+        let protected = protected.clone();
+        let i = i;
+        threads.push(thread::spawn(move || {
+            let mut last_val = 0;
+            for _ in 0..iterations {
+                {
+                    let mut val = protected.write();
+                    if debug_locks { println!("downgrader {i} write-locked"); }
+                    *val += 1;
+
+                    let val = val.downgrade();
+                    if debug_locks { println!("downgrader {i} downgraded"); }
+                    if *val < last_val {
+                        panic!("Error in downgrader: value went from {last_val} to {val}", val = *val);
+                    }
+                    last_val = *val;
+                    if debug_locks { println!("downgrader {i} read-unlocked"); }
+                }
+                thread::sleep(sleep_time);
+            }
+            if debug_exits { println!("downgrader {i} exited"); }
+        }))
+    }
+
+    for i in 0..num_upgraders {
+        let protected = protected.clone();
+        let i = i;
+        let upgrade_timeouts = upgrade_timeouts.clone();
+        threads.push(thread::spawn(move || {
+            let mut last_val = 0;
+            for _ in 0..iterations {
+                {
+                    let val = read_eventually(&protected, read_timeout);
+                    if debug_locks { println!("upgrader {i} read-locked"); }
+                    if *val < last_val {
+                        panic!("Error in reader: value went from {last_val} to {val}", val = *val);
+                    }
+                    last_val = *val;
+
+                    let val = val.try_upgrade(upgrade_timeout);
+                    match val {
+                        LockUpgradeOutcome::Upgraded(mut val) => {
+                            if debug_locks { println!("upgrader {i} upgraded"); }
+                            *val += 1;
+                            if debug_locks { println!("upgrader {i} write-unlocked"); }
+                        },
+                        LockUpgradeOutcome::Unchanged(_) => {
+                            upgrade_timeouts.fetch_add(1, Ordering::Relaxed);
+                            if debug_locks { println!("upgrader {i} upgrade timed out"); }
+                        }
+                    }
+                }
+                thread::sleep(sleep_time);
+            }
+            if debug_exits { println!("upgrader {i} exited"); }
+        }))
+    }
+
+    for thread in threads {
+        thread.join().unwrap();
+    }
+    let upgrade_timeouts = upgrade_timeouts.load(Ordering::Relaxed);
+    let expected_writes = ((num_writers + num_downgraders + num_upgraders) * iterations) as u64 - upgrade_timeouts;
+    assert_eq!(expected_writes, *protected.read());
+
+    let time_taken = (Instant::now() - start_time).as_secs_f64();
+    let ops = (num_readers + num_writers + 2 * num_downgraders + 2 * num_upgraders) * iterations;
+    let rate = (ops as f64) / time_taken;
+    println!("{ops} ops took {time_taken:.3} seconds; {rate:.3} ops/s");
+    println!("upgrade timeouts: {upgrade_timeouts:?}");
 }
