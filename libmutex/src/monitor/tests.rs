7ccaf9f5ed84a1c7530d424d610c8d7efd073b65
Emil Koutanov <ekoutanov@gmail.com>
2022-08-28 09:19:01 +1000

    Notify chain

--- libmutex/src/monitor/tests.rs
+++ libmutex/src/monitor/tests.rs
@@ -267,6 +267,84 @@ fn wait_for_notify_all() {
     }
 }
 
+#[test]
+fn wait_notify_chain() {
+    let monitor = Arc::new(SpeculativeMonitor::new(1u8));
+
+    let t_2_awoken = Arc::new(AtomicBool::new(false));
+    let t_2 = {
+        let monitor = monitor.clone();
+        let t_2_awoken = t_2_awoken.clone();
+        test_utils::spawn_blocked(move || {
+            monitor.enter(|val| {
+                match val {
+                    2 => {
+                        t_2_awoken.store(true, std::sync::atomic::Ordering::Relaxed);
+                        *val += 1;
+                        Directive::NotifyAll
+                    },
+                    _ => Directive::Wait(Duration::MAX)
+                }
+            })
+        })
+    };
+
+    let t_3_awoken = Arc::new(AtomicBool::new(false));
+    let t_3 = {
+        let monitor = monitor.clone();
+        let t_3_awoken = t_3_awoken.clone();
+        test_utils::spawn_blocked(move || {
+            monitor.enter(|val| {
+                match val {
+                    3 => {
+                        t_3_awoken.store(true, std::sync::atomic::Ordering::Relaxed);
+                        *val += 1;
+                        Directive::NotifyOne // one remaining thread
+                    },
+                    _ => Directive::Wait(Duration::MAX)
+                }
+            })
+        })
+    };
+
+    let t_4_awoken = Arc::new(AtomicBool::new(false));
+    let t_4 = {
+        let monitor = monitor.clone();
+        let t_4_awoken = t_4_awoken.clone();
+        test_utils::spawn_blocked(move || {
+            monitor.enter(|val| {
+                match val {
+                    4 => {
+                        t_4_awoken.store(true, std::sync::atomic::Ordering::Relaxed);
+                        *val += 1;
+                        Directive::Return // no one left to notify
+                    },
+                    _ => Directive::Wait(Duration::MAX)
+                }
+            })
+        })
+    };
+
+    // initially, t_2-4 are parked
+    assert!(!t_2.is_finished());
+    assert!(!t_3.is_finished());
+    assert!(!t_4.is_finished());
+
+    monitor.wait_for_num_waiting(Ordering::is_eq, 3, LONG_WAIT).unwrap();
+
+    // once the value is set to 2, t_2 should wake and cascade through all others
+    monitor.enter(|val| {
+        *val = 2; // this trips t_2
+        Directive::NotifyAll
+    });
+
+    // eventually, all threads are woken and leave the monitor
+    monitor.wait_for_num_waiting(Ordering::is_eq, 0, LONG_WAIT).unwrap();
+    t_2.join().unwrap();
+    t_3.join().unwrap();
+    t_4.join().unwrap();
+}
+
 impl<T> SpeculativeMonitor<T> {
     fn wait_for_num_waiting(&self, cmp: impl FnMut(Ordering) -> bool, target: u32, duration: Duration) -> WaitResult {
         wait::Spin::wait_for_inequality(|| self.num_waiting(), cmp, &target, duration)
