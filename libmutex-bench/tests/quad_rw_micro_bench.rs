39fe4dccd255b5082fcae58d71ede97c8c5bdd84
Emil Koutanov <ekoutanov@gmail.com>
2022-08-30 15:24:51 +1000

    Rename

--- libmutex-bench/tests/quad_rw_micro_bench.rs
+++ libmutex-bench/tests/quad_rw_micro_bench.rs
@@ -6,66 +6,66 @@ use libmutex_bench::quad_harness;
 use libmutex_bench::quad_harness::{Addable, BoxedInt, ExtendedOptions, Options};
 
 #[test]
-fn quake_micro_bench_read_biased_int() {
-    __quake_micro_bench::<i64, XLock<_, ReadBiased>>();
+fn quad_micro_bench_read_biased_int() {
+    __quad_micro_bench::<i64, XLock<_, ReadBiased>>();
 }
 
 #[test]
-fn quake_micro_bench_read_biased_boxed_int() {
-    __quake_micro_bench::<BoxedInt, XLock<_, ReadBiased>>();
+fn quad_micro_bench_read_biased_boxed_int() {
+    __quad_micro_bench::<BoxedInt, XLock<_, ReadBiased>>();
 }
 
 #[test]
 fn quad_micro_bench_read_biased_string() {
-    __quake_micro_bench::<String, XLock<_, ReadBiased>>();
+    __quad_micro_bench::<String, XLock<_, ReadBiased>>();
 }
 
 #[test]
-fn quake_micro_bench_write_biased_int() {
-    __quake_micro_bench::<i64, XLock<_, WriteBiased>>();
+fn quad_micro_bench_write_biased_int() {
+    __quad_micro_bench::<i64, XLock<_, WriteBiased>>();
 }
 
 #[test]
-fn quake_micro_bench_write_biased_boxed_int() {
-    __quake_micro_bench::<BoxedInt, XLock<_, WriteBiased>>();
+fn quad_micro_bench_write_biased_boxed_int() {
+    __quad_micro_bench::<BoxedInt, XLock<_, WriteBiased>>();
 }
 
 #[test]
-fn quake_micro_bench_write_biased_string() {
-    __quake_micro_bench::<String, XLock<_, WriteBiased>>();
+fn quad_micro_bench_write_biased_string() {
+    __quad_micro_bench::<String, XLock<_, WriteBiased>>();
 }
 
 #[test]
-fn quake_micro_bench_arrival_ordered_int() {
-    __quake_micro_bench::<i64, XLock<_, ArrivalOrdered>>();
+fn quad_micro_bench_arrival_ordered_int() {
+    __quad_micro_bench::<i64, XLock<_, ArrivalOrdered>>();
 }
 
 #[test]
-fn quake_micro_bench_arrival_ordered_boxed_int() {
-    __quake_micro_bench::<BoxedInt, XLock<_, ArrivalOrdered>>();
+fn quad_micro_bench_arrival_ordered_boxed_int() {
+    __quad_micro_bench::<BoxedInt, XLock<_, ArrivalOrdered>>();
 }
 
 #[test]
-fn quake_micro_bench_arrival_ordered_string() {
-    __quake_micro_bench::<String, XLock<_, ArrivalOrdered>>();
+fn quad_micro_bench_arrival_ordered_string() {
+    __quad_micro_bench::<String, XLock<_, ArrivalOrdered>>();
 }
 
 #[test]
-fn quake_micro_bench_stochastic_int() {
-    __quake_micro_bench::<i64, XLock<_, Stochastic>>();
+fn quad_micro_bench_stochastic_int() {
+    __quad_micro_bench::<i64, XLock<_, Stochastic>>();
 }
 
 #[test]
-fn quake_micro_bench_stochastic_boxed_int() {
-    __quake_micro_bench::<BoxedInt, XLock<_, Stochastic>>();
+fn quad_micro_bench_stochastic_boxed_int() {
+    __quad_micro_bench::<BoxedInt, XLock<_, Stochastic>>();
 }
 
 #[test]
-fn quake_micro_bench_stochastic_string() {
-    __quake_micro_bench::<String, XLock<_, Stochastic>>();
+fn quad_micro_bench_stochastic_string() {
+    __quad_micro_bench::<String, XLock<_, Stochastic>>();
 }
 
-fn __quake_micro_bench<T: Addable, L: for<'a> LockSpec<'a, T = T> + 'static>() {
+fn __quad_micro_bench<T: Addable, L: for<'a> LockSpec<'a, T = T> + 'static>() {
     let opts = Options {
         readers: 4,
         writers: 4,
