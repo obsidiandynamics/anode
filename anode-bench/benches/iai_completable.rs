0b95eee2dc5cfce71c93ed1fc4c1df66035b5c41
Emil Koutanov <ekoutanov@gmail.com>
2022-09-02 12:05:52 +1000

    More benches

--- anode-bench/benches/iai_completable.rs
+++ anode-bench/benches/iai_completable.rs
@@ -1,6 +1,33 @@
 use std::time::Duration;
-use anode::completable::Completable;
+use anode::completable::{Completable, Completed};
 use iai::{black_box, main};
+
+fn incomplete_complete() -> Option<()> {
+    let completable = Completable::default();
+    let completed = completable.complete(());
+    debug_assert!(completed.is_none());
+    completed
+}
+
+fn incomplete_is_complete() -> bool {
+    let completable = Completable::<()>::default();
+    let completed = completable.is_complete();
+    debug_assert!(!completed);
+    completed
+}
+
+fn completed_complete() -> Option<()> {
+    let completable = Completable::new(());
+    let completed = completable.complete(());
+    debug_assert!(completed.is_some());
+    completed
+}
+
+fn completed_get() {
+    let completable = Completable::new(());
+    let completed = completable.get();
+    black_box(completed);
+}
 
 fn incomplete_try_get() {
     let completable = Completable::<()>::default();
@@ -9,4 +36,4 @@ fn incomplete_try_get() {
     black_box(maybe_completed);
 }
 
-main!(incomplete_try_get);
+main!(incomplete_complete, incomplete_is_complete, completed_complete, completed_get, incomplete_try_get);
