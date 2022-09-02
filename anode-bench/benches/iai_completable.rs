7548240fd4f348ebced678200d3c40fd2df7c083
Emil Koutanov <ekoutanov@gmail.com>
2022-09-02 11:59:25 +1000

    Added blackbox

--- anode-bench/benches/iai_completable.rs
+++ anode-bench/benches/iai_completable.rs
@@ -1,11 +1,12 @@
 use std::time::Duration;
 use anode::completable::Completable;
-use iai::main;
+use iai::{black_box, main};
 
 fn incomplete_try_get() {
     let completable = Completable::<()>::default();
     let maybe_completed = completable.try_get(Duration::ZERO);
     debug_assert!(maybe_completed.is_none());
+    black_box(maybe_completed);
 }
 
 main!(incomplete_try_get);
