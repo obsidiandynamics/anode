c7bc35f0eae9697bb83c37f9c7dda7f0407531a9
Emil Koutanov <ekoutanov@gmail.com>
2022-09-04 08:56:02 +1000

    Fixed benchmark name

--- anode-bench/benches/cri_rand.rs
+++ anode-bench/benches/cri_rand.rs
@@ -34,7 +34,7 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("rand/next_range<u128>/small", |b| {
         b.iter(|| rand.gen_range(0..17u128));
     });
-    c.bench_function("xorshift/next_range<u128>/large", |b| {
+    c.bench_function("rand/next_range<u128>/large", |b| {
         b.iter(|| rand.gen_range(0..1u128 << 80));
     });
     c.bench_function("rand/next_bool", |b| {
